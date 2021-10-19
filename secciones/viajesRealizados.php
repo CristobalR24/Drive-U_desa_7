8c18d9915f0e314b25782cdb7ea7995fe0e8a2a7
Cristobal_Rodriguez <cristobalr2400@gmail.com>
2021-10-18 19:59:34 -0500

    boton regresar

--- secciones/viajesRealizados.php
+++ secciones/viajesRealizados.php
@@ -225,7 +225,7 @@
 <!-- partial -->
   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="../js/script.js"></script>
 <br><br>
-<button type="button" class="btn">Regresar</button>
+<button href="../index.php" type="button" class="btn">Regresar</button>
 
 <footer>
     <br>
