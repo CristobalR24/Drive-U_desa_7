a975201cc80389542260466e536030b451f648bc
ElianysG <86267746+ElianysG@users.noreply.github.com>
2021-12-04 20:28:42 -0500

    viajes realizados

--- secciones/viajesRealizados.php
+++ secciones/viajesRealizados.php
@@ -1,3 +1,9 @@
+<?php 
+    include("conexion/conexion.php");
+    $consulta = $con->query('SELECT * FROM viajes');
+   // var_dump($consulta); 
+?>
+
 <!DOCTYPE html>
 <html lang="en" >
 <head>
@@ -5,44 +11,31 @@
   <title>Drive-U - Viajes realizados</title>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <meta name="viewport" content="width=device-width"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-<link rel="stylesheet" href="../css/estilo_ViajesRealizados.css">
+  <link rel="stylesheet" href="estilo/estilo_ViajesRealizados.css">
 
 </head>
 <body>
   <header>
-    <div class="logo"><img src="../imagenes/logo_utp.png" width="120px"></div> 
+    <div class="logo"><img src="imagenes/logo_utp.png" width="120px"></div> 
     <span class="titulo">Drive-U</span></header>
     
     <nav>
-    <ul>
-		  <li><a href="../index.php">Inicio</a></li>
-      <li><a href="preguntas_frecuentes.php">Preguntas frecuentes</a></li>
-			<li><a href="#">Servicios</a>
-        <div class="submenu">
-          <ul><a href="crear_solicitud.php">Crear solicitud</a></ul>
-          <ul><a href="estado_solicitud.php">Estado de solicitud</a></ul>
-        </div>
-      </li>
-
-      <li><a href="#">Opciones de administrador</a>
-        <div class="submenu">
-          <ul><a href="crear_usuario.php">Registrar usuario</a></ul>
-          <ul><a href="actualizar_elim_usuario.php">Modificar usuarios</a></ul>
-          <ul><a href="estado_vehiculos.php">Lista de vehiculos</a></ul>
-          <ul><a href="aceptar_solicitud.php">Procesar solicitudes</a></ul>
-        </div>
-      </li>
-
-      <li><a href="#">Opciones de conductor</a>
-        <div class="submenu">
-          <ul><a href="viajespendientes.php">Viajes pendientes</a></ul>
-          <ul><a href="viajesRealizados.php">Viajes realizados</a></ul>
-        </div>
-      </li>
-      <!-- tambien sera opcion de salir --> 
-			<li class="derecha"><a href="login.php">Iniciar sesión <span class="material-icons pequeno">home</span> </a></li>	
-  	</ul>
-   </nav>
+     <ul>
+       <li><a href="#">Inicio</a></li>
+       <li><a href="#">Nosotros</a></li>
+       <li><a href="#">Trámites</a>
+         <div class="submenu">
+           <ul><a href="#">Viajes realizados</a></ul>
+           <ul><a href="#">Viajes pendientes</a></ul>
+           <ul><a href="#">Informe de viajes</a></ul>
+         </div>
+       </li>
+       
+       <li><a href="#">Preguntas frecuentes</a></li>
+       <li><a href="#">Contáctenos</a></li>
+       <li class="derecha"><a href="#">Cerrar sesión <span class="material-icons pequeno">home</span> </a></li>	
+     </ul>
+    </nav>
     <BR><BR>
 <!-- partial:index.partial.html -->
 <div class="posicion">
@@ -53,179 +46,27 @@
 
 <ul class="surveys grid">
  
-  <a href="#">
-    <li class="survey-item">
-    
-      <span class="survey-country list-only">
-        UK
-      </span>
-      
-      <span class="survey-name">
-        UK Beer May 2014
-      </span>
-      
-      <span class="survey-country grid-only">
-        UK
-      </span>
-      
-      <div class="pull-right">
-        
-         <span class="survey-end-date ended">
-          2014 - May 10
-        </span>
-        
-      </div>
-    </li>
-  </a>
-  <a href="#">
-    <li class="survey-item">
-    
-      <span class="survey-country list-only">
-        UK
-      </span>
-      
-      <span class="survey-name">
-        UK Beer May 2014
-      </span>
-      
-      <span class="survey-country grid-only">
-        UK
-      </span>
-      
-      <div class="pull-right">
-        
-         <span class="survey-end-date ended">
-          2014 - May 10
-        </span>
-        
-      </div>
-    </li> 
-  </a>
-  <a href="#">
-    <li class="survey-item">
-    
-      <span class="survey-country list-only">
-        UK
-      </span>
-      
-      <span class="survey-name">
-        UK Beer May 2014
-      </span>
-      
-      <span class="survey-country grid-only">
-        UK
-      </span>
-      
-      <div class="pull-right">
-        
-         <span class="survey-end-date ended">
-          2014 - May 10
-        </span>
-        
-      </div>
-    </li>
-  </a>
-  <a href="#">
-    <li class="survey-item">
-    
-      <span class="survey-country list-only">
-        UK
-      </span>
-      
-      <span class="survey-name">
-        UK Beer May 2014
-      </span>
-      
-      <span class="survey-country grid-only">
-        UK
-      </span>
-      
-      <div class="pull-right">
-        
-         <span class="survey-end-date ended">
-          2014 - May 10
-        </span>
-        
-      </div>
-    </li>
-  </a>
-  <a href="#">
-    <li class="survey-item">
-    
-      <span class="survey-country list-only">
-        UK
-      </span>
-      
-      <span class="survey-name">
-        UK Beer May 2014
-      </span>
-      
-      <span class="survey-country grid-only">
-        UK
-      </span>
-      
-      <div class="pull-right">
-        
-         <span class="survey-end-date ended">
-          2014 - May 10
-        </span>
-        
-      </div>
-    </li>
-  </a>
-  <a href="#">
-    <li class="survey-item">
-    
-      <span class="survey-country list-only">
-        UK
-      </span>
-      
-      <span class="survey-name">
-        UK Beer May 2014
-      </span>
-      
-      <span class="survey-country grid-only">
-        UK
-      </span>
-      
-      <div class="pull-right">
-        
-         <span class="survey-end-date ended">
-          2014 - May 10
-        </span>
-        
-      </div>
-    </li>
-  </a>
-  <a href="#">
-    <li class="survey-item">
-    
-      <span class="survey-country list-only">
-        UK
-      </span>
-      
-      <span class="survey-name">
-        UK Beer May 2014
-      </span>
-      
-      <span class="survey-country grid-only">
-        UK
-      </span>
-      
-      <div class="pull-right">
-        
-         <span class="survey-end-date ended">
-          2014 - May 10
-        </span>
-        
-      </div>
-    </li>
-  </a>
+  <?php
+    while($i = $consulta->fetch(PDO::FETCH_OBJ)){ ?>
+     <a href="#"> 
+      <li class="survey-item">
+        <span class="survey-name"> ID Viaje: <?php echo $i->id_viaje; ?></span> <br>         
+        <span class="survey-name"> Fecha: <?php echo $i->fecha; ?> </span>
+        <span class="survey-name"> Destino: <?php echo $i->destino; ?> </span><br>
+      <div class="pull-right">  
+        <span class="survey-name">Vehículo: <?php echo $i->vehiculo; ?> </span>
+        <span class="survey-name"> Cantidad de personas: <?php echo $i->cantidad_personas; ?> </span>
+        <span class="survey-name"> ID Chofer <?php echo $i->id_chofer; ?> </span><br>
+        <span class="survey-name"> Estado: <?php echo $i->estado; ?> </span>  </div>
+     </li>  
+    <?php  } ?>         
+    </a>
+
 </ul>
 <!-- partial -->
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="../js/script.js"></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
 <br><br>
-<button href="../index.php" type="button" class="btn">Regresar</button>
+<button type="button" class="btn">Regresar</button>
 
 <footer>
     <br>
